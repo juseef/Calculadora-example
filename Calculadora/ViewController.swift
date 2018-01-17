f86207df82a0c4ab35900a018a1d2a845ad46a11
Jose Flores <Jose@Oscars-MacBook-Pro.local>
2018-01-17 14:23:53 +0100

    botones de números conectados y botón de borrado en proceso

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -11,17 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var lblDisplay: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Cuando se llama a esta función, se actualiza el TextField de la interfaz con el texto del boton pulsado
+    @IBAction func btnPulsado(_ sender: UIButton) {
+        let texto = sender.titleLabel!.text!
+        print("\(texto)")
+        actualizarVista(texto:texto)
+        
     }
-    
+   //Esta función se encarga de mostrar los cambios del modelo en la interfaz
+    func actualizarVista(texto : String){
+        print("\(texto)")
+        lblDisplay.insertText(texto)
+        
+    }
 
+    @IBAction func bntBorrar(_ sender: UIButton) {
+        let nuevoTexto = lblDisplay.text![..<(lblDisplay.text!.endIndex - 1)]
+        
+        lblDisplay.text = String(nuevoTexto)
+    }
 }
 
