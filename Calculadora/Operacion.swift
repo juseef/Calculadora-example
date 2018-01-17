9c8298d8fc51a06cfb84d6f4a667feba36460740
Jose Flores <Jose@Oscars-MacBook-Pro.local>
2018-01-17 12:41:59 +0100

    creando clase operación

---
+++ Calculadora/Operacion.swift
@@ -0,0 +1,17 @@
+//
+//  Operacion.swift
+//  Calculadora
+//
+//  Created by Jose Flores on 17/01/2018.
+//  Copyright © 2018 Jose Flores. All rights reserved.
+//
+
+import Foundation
+
+class Operacion{
+    
+    var cadena: String!
+    var array: Array<Float>!
+    var flotante: Float!
+    
+}
