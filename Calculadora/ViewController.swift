1c5d514f39338a612aa8e83c6bd417a6e5ca224f
Jose Flores <Jose@Oscars-MacBook-Pro.local>
2018-01-18 20:25:13 +0100

    Calculadora terminada 18/01/2018 20:25

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -19,20 +19,24 @@ class ViewController: UIViewController {
     var n1:Float!
     //var para permitir seguir con la operacion
     var permisoConcedido = false
+    
     //Cuando se llama a esta función, se actualiza el TextField de la interfaz con el texto del boton pulsado
     @IBAction func btnPulsado(_ sender: UIButton) {
         var texto = sender.titleLabel!.text!
         print("\(texto)")
         actualizarVista(texto:texto)
     }
-    //funcion que contiene los botones de operaciones
+    //funcion que contiene los botones de las operaciones
     @IBAction func btn_operaciones(_ sender: UIButton) {
+        //comprobacion de si esta vacio el textfield
         if(lblDisplay.text == ""){
+        //si esta vacio se tomará como un 0
             n1 = 0
         }else{
-            
+            //si no esta vacio se comprueba si hay un punto solo
             if (lblDisplay.text != "."){
             n1 = Float(lblDisplay.text!)!
+                // en caso de que llegue aqui se permite realizar la operacion
                 permisoConcedido = true
             }else{
                 permisoConcedido = false
@@ -88,13 +92,12 @@ class ViewController: UIViewController {
         lblDisplay.insertText(texto)
         
     }
-
+    //función que se encarga de borrar el textfield
     @IBAction func bntBorrar(_ sender: UIButton) {
         if (lblDisplay.text != ""){
-        let texto:String = (lblDisplay.text)!
-        let nuevoTexto = texto.substring(to: texto.index(before: texto.endIndex))
-        
-        lblDisplay.text = nuevoTexto
+                let texto:String = (lblDisplay.text)!
+                let nuevoTexto = texto.substring(to: texto.index(before: texto.endIndex))
+                lblDisplay.text = nuevoTexto
         }else{
             print("textfield vacio")
         }
